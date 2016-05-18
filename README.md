067259022981b8e14bcfeae4eb809ede85c9a77d
Anthony Headley <hoss@downrighttech.com>
2016-05-18 12:30:53 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# OnHub Local Status
+This PHP web application should be installed on a PHP enabled web server on the same network segment as your OnHub LAN connection. Once installed the application will query the state of the OnHub status API and display the results in a clean material design layout.
+
+If you would like to test the page without access to an OnHub edit the following lines in `index.php`:
+
+```
+<?php
+   $serverStatusUrl = "http://192.168.86.1/api/v1/status"; //OnHub address
+   // $serverStatusUrl = "status.json";                       //testing without OnHub
+?>
+```
+
+#Features:
+* Uses [Material Design Lite](https://getmdl.io/index.html) to format page in material design
+* Converts uptime from `seconds` to `xx days xx hours xx minutes xx seconds`
+* Converts boolean statuses to color coded sensible values (Online, Set, Up ...)
+
+#TODO:
+* Currently dumps the whole status JSON with minimal modifications, should better display the status to user.
+* Allow the local server to log any outages (LAN or WAN on the OnHUB).
+* Allow the server to email or otherwise notify the user when events happen such as when the network goes down and has come back up, or when the OnHub is updating.
+* Headers / Footers and things.
+* Keys need display values ("updateRequired" == "Update Required")
+* The Root display of "Wan" should say "WAN"
+
+![screen shot](https://github.com/hossimo/OnHubLocalStatus/blob/master/screenshot.png)
---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<?php
+    $serverStatusUrl = "http://192.168.86.1/api/v1/status"; //OnHub address
+    // $serverStatusUrl = "status.json";                    //testing without OnHub
+?>
+<html>
+<head>
+    <!-- load css resources -->
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+    <!-- load material design light resources -->
+    <link rel="stylesheet" type="text/css" href="./mdl/material.min.css">
+    <script src="./mdl/material.min.js"></script>
+
+    <!-- load fonts if we can -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <?php
+    $json = file_get_contents($serverStatusUrl);
+    $onHubStatus = json_decode($json);
+
+    $softwave = $onHubStatus->software;
+    $system = $onHubStatus->system;
+    $wan = $onHubStatus->wan;
+
+    $version = $onHubStatus->software->softwareVersion;
+    $wanLink = $onHubStatus->wan->etherLink;
+    $wanOnline = $onHubStatus->wan->online;
+    $systemUptime = $onHubStatus->system->uptime;
+    function format($test, $true, $ifTrue, $ifFalse) {
+        $retVal = ($test == $true) ? '[<span class="green">' . $ifTrue .'</span>]' : '[<span class="red">' . $ifTrue .'</span>]' ;
+        return $retVal;
+    }
+
+    function ss2dhm ($s){
+        $days = floor($s / (60*60*24));
+        $hours = floor(($s - ($days*60*60*24)) / (60*60));
+        $mins = floor(($s - ($days*60*60*24) - ($hours*60*60)) / 60);
+        $secs = ($s -($days*60*60*24) - ($hours*60*60) - ($mins*60));
+        return $days . " days " .$hours. " hours " .$mins. " minutes ".$secs. " seconds ";
+    }
+    ?>
+    <meta charset="UTF-8">
+    <?php $titleStat = ($wanOnline == "1") ? 'Online' : 'Offline' ;?>
+    <title>onHub Status - <?php echo $titleStat ?></title>
+</head>
+
+<body>
+
+    <!-- Display Cards -->
+    <?php foreach ($onHubStatus as $oKey => $oValue) { ?>
+        <div class="mdl-card card-wide mdl-shadow--16dp">
+            <div class="mdl-card__title">
+                <h2 class="mdl-card__title-text"><?php echo $oKey ?></h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+                <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                    <tbody>
+                        <?php
+                        foreach ($oValue as $key => $value) {
+                            switch ($key){
+                                case "uptime":
+                                $v = ss2dhm($value);
+                                break;
+                                case "ethernetLink":
+                                $v = format($value, "1", "Up", "Down");
+                                break;
+                                case "online":
+                                $v = format($value, "1", "Online", "Offline");
+                                break;
+                                case "ipAddress":
+                                $v = format($value, "1", "Set", "Not Set");
+                                break;
+                                default:
+                                $v = $value;
+                            }
+
+                            echo '<tr>';
+                            echo '<td class="mdl-data-table__cell--non-numeric">'. $key .'</td>';
+                            echo '<td >'. $v .'</td>';
+                            echo '</tr>';
+                        } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <?php } ?>
+    </body>
+    </html>
